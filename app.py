dc694b3bf5fafdc6770eb2bd9eb717e41f71f008
astella yip <astellayip@Angel-2.local>
2026-01-07 22:59:28 +0800

    Initial commit

--- app.py
+++ app.py
@@ -1,5 +1,13 @@
-#streamlit frontend 
+#UI streamlit frontend 
 import streamlit as st
+import xrpl_utils #blockchain logic
 
 st.title("finetech NFT")
 st.write("Welcome to finetech NFT")
+
+#Calls function to connect to XRPL
+client = xrpl_utils.connect_xrpl()  # Connect to XRPL testnet
+st.write("Connected to XRPL testnet")  # Show connection
+
+if st.button("Mint NFT"):
+    st.write("Minting NFT...")
---
+++ xrpl_utils.py
@@ -0,0 +1,2 @@
+def connect_xrpl():
+    return True
